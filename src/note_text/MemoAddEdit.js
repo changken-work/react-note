7885a0168276212d42b45259352318f4f5bcf7fc
winnieywhsiao <49788032+winnieywhsiao@users.noreply.github.com>
2020-12-13 22:27:47 +0800

    memo刪除完成

--- src/note_text/MemoAddEdit.js
+++ src/note_text/MemoAddEdit.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import { Header, ListItem, Icon } from 'react-native-elements';
-import { SafeAreaView, TextInput, View, Button, TouchableOpacity, Modal, LogBox } from "react-native";
+import { SafeAreaView, TextInput, View, Button, TouchableOpacity, Modal, LogBox, Text } from "react-native";
 import { Menu, Divider, Provider } from 'react-native-paper';
 
 import styles from '../styles';
@@ -9,9 +9,12 @@ import * as firebase from 'firebase';
 import firestore from 'firebase/firestore';
 import * as FirebaseCore from 'expo-firebase-core';
 
+import DeleteMenu from "./DeleteMenu";
+
 export default function MemoAddEdit(props) {  
   LogBox.ignoreLogs(['Setting a timer']);
 
+  const [menuVisible, setMenuVisible] = useState(false);
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
 
@@ -60,14 +63,29 @@ export default function MemoAddEdit(props) {
     const docRef = await db.collection("users").doc("MeRcqDluKIWS1jjvmiN8").collection("notes").doc(id).set({
       title: title,
       content: content
-    });
-    console.log("update success!");
+    })
+    .then(function() {
+        console.log("update success!");
+    })
+    .catch(function(error) {
+        console.error("Error writing document: ", error);
+    });    
   }
 
   function cancel(){
     setTitle("");
     setContent("");
     props.hide();
+  }
+
+  async function deleteMemo(id){
+    // console.log(id + " delete");
+    const docRef = await db.collection("users").doc("MeRcqDluKIWS1jjvmiN8").collection("notes").doc(id).delete().then(function() {
+        console.log(id + " successfully deleted!");
+        props.hide();
+    }).catch(function(error) {
+        console.error("Error removing document: ", error);
+    });
   }
 
   return (
@@ -88,9 +106,10 @@ export default function MemoAddEdit(props) {
           }
           centerComponent={{ text: 'Memo', style: { color: '#fff', fontSize: 20 } }}
           rightComponent={
+            // checkActions()
             <TouchableOpacity
               onPress={() => {
-                console.log("Open menu");
+                deleteMemo(props.id)
               }}
             >
               <Icon name="more-vert" color='#fff' />
@@ -117,6 +136,5 @@ export default function MemoAddEdit(props) {
       </Modal>
     </SafeAreaView>
 
-    
  );
 }
